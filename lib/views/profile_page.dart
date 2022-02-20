45831b6887f15ae96e509b8354773ed8e18b55ce
Deniz Durmaz <87248460+Denizdurmazz@users.noreply.github.com>
2022-02-20 22:55:46 +0300

    work

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -9,17 +12,33 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final ImagePicker _picker=ImagePicker();
+  late final  XFile? image;
+Future<void> getImageFromCamera()async{
+  image=await _picker.pickImage(source: ImageSource.camera);
+  setState(() {
+    
+  });
+
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("My Profile"),backgroundColor: Colors.white,),
       body: ListView(
 
-        children:const [
+        children: [
+          const SizedBox(height: 20,),
           CircleAvatar(
-            child: Text("Foto"),
+            child: image==null?Text("Foto"):Image.file(File(image!.path)),
             radius: 80,
-          )
+          ),
+          ElevatedButton(onPressed: (){
+            getImageFromCamera();
+          }, child: const Text("Select"))
+
         ],
       ),
       
